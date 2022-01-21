f13634c24dc63fe5fbc151ca6829fbe70409c7d5
OZHER <75246845+OZHER@users.noreply.github.com>
2022-01-21 23:12:50 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,17 +3,21 @@ import sys, pygame
 from classes import Color, Board
 pygame.init()
 
-SCRWIDTH, SCRHGHT, RUNNING = 1600, 900, True
+SCRWIDTH, SCRHGHT, RUNNING = 1600, 800, True
+BOARDWIDTH, BOARDHGTH = 800, 800
+SQUARESIZE = BOARDWIDTH // 8
 backgroundColor = Color(0, 0, 0, 0)
 
 display = pygame.display.set_mode((SCRWIDTH, SCRHGHT))
-board = Board(SCRWIDTH, SCRHGHT)
+board = Board(BOARDWIDTH, BOARDHGTH)
+board.loadPieces(SQUARESIZE)
 
 while RUNNING:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
         #events
-    
     display.fill(backgroundColor.getRGB())
-    board.display(display)
+    board.displayBoard(display)
+
+    board.displayPieces(display, SQUARESIZE)
     pygame.display.flip()
